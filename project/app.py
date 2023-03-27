0bbbac48730d403ad6adb8a4288b7e08443b04f3
Thakur <Sumit.Thakur@ingrammicro.com>
2023-03-27 16:11:39 +0530

    env variable

--- project/app.py
+++ project/app.py
@@ -68,4 +68,4 @@ def create():
     return jsonify({"Response": response.response})
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="https://chatcustomgpt-production.up.railway.app/", port=5000)
