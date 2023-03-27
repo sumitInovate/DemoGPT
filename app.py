1a18caef78090e1fb0d275209c81d123ba71ce33
Thakur <Sumit.Thakur@ingrammicro.com>
2023-03-27 10:29:42 +0530

    update app

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def create():
     return jsonify({"Response": response.response})
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True, host="0.0.0.0", port=5000)
